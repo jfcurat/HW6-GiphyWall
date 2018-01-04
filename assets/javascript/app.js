d3d54c585514d73bceff9bcef384f1d88dedfdab
James Curatalo <jfcurat@gmail.com>
2018-01-04 02:28:11 -0800

    Starting Point
    setup placeholder html page
    got placeholder pizzaSurfer gif by googling 'gif'
    started thinking about topics to include - commented them in
    commented in the giphy queryURL to dial in later
    ready to gif

---
+++ assets/javascript/app.js
@@ -0,0 +1,17 @@
+//array of strings - to be used as gif genres
+//possible topics: music (general), guitar, cats, dogs, food, videogames, tech, camping, hiking, fishing, fish?
+var topics = ["topic1", "topic2", "topic3", "topic4", "topic5", "topic6", "topic7", "topic8", "topic9", "topic10"];
+
+
+/*
+var queryURL = 'https://api.giphy.com/v1/gifs/trending?q=(string search term or+search+terms)
+				&limit=(int number of results to return) &rating=(string limit results to rating y,g,pg,pg-13,r)
+				&api_key=nvNb9MHt4xzv60kubg11U9BF7A6Mkr92';
+
+$.ajax({
+	url: queryURL,
+	method: 'GET'
+}).done(function(response) {
+	console.log(response);
+});
+*/
