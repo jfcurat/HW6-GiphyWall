0c6f07c445b1b7fd574749331639ba542f291267
James Curatalo <jfcurat@gmail.com>
2018-01-04 02:50:01 -0800

    Minor Edits
    forgot i wasnted to try to favor single quotes over double quotes for this assignment
    went thru and changed them
    made a favicon

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,6 @@
 //array of strings - to be used as gif genres
 //possible topics: music (general), guitar, cats, dogs, food, videogames, tech, camping, hiking, fishing, fish?
-var topics = ["topic1", "topic2", "topic3", "topic4", "topic5", "topic6", "topic7", "topic8", "topic9", "topic10"];
+var topics = ['topic1', 'topic2', 'topic3', 'topic4', 'topic5', 'topic6', 'topic7', 'topic8', 'topic9', 'topic10'];
 
 
 /*
