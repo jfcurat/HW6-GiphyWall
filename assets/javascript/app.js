4340fa1a946fa7d00be0eaf4a083201dcb007d40
James Curatalo <jfcurat@gmail.com>
2018-01-06 19:51:11 -0800

    Separated API Query String into Variables
    other placeholder stuff
    added button area. restyle later
    added submit form. restyle later

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,11 +2,20 @@
 //possible topics: music (general), guitar, cats, dogs, food, videogames, tech, camping, hiking, fishing, fish?
 var topics = ['topic1', 'topic2', 'topic3', 'topic4', 'topic5', 'topic6', 'topic7', 'topic8', 'topic9', 'topic10'];
 
+//jQuery to save the value from the input box
+var boxTerm = $("#id-of-input-box").val().trim();
 
-/*
-var queryURL = 'https://api.giphy.com/v1/gifs/trending?q=(string search term or+search+terms)
-				&limit=(int number of results to return) &rating=(string limit results to rating y,g,pg,pg-13,r)
-				&api_key=nvNb9MHt4xzv60kubg11U9BF7A6Mkr92';
+//break out the query string parameters into vars
+var url = 'https://api.giphy.com/v1/gifs/search?';
+var apiKey = 'api_key=nvNb9MHt4xzv60kubg11U9BF7A6Mkr92';
+var searchTerm = '&q=' + boxTerm; //use + for spaces
+var gifsAmount = "&limit=10";
+
+var queryURL = url + apiKey + searchTerm + gifsAmount; 
+console.log(queryURL);
+
+//don't use rating in search, but display above each gif.
+//&rating=(string, limits results to rating y,g,pg,pg-13,r)
 
 $.ajax({
 	url: queryURL,
@@ -14,4 +23,3 @@ $.ajax({
 }).done(function(response) {
 	console.log(response);
 });
-*/
